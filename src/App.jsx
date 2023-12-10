a3aafb0e386f4c9de2f4064d1494c158ee23413a
Imran Ahmed <iam.imran@yahoo.com>
2023-12-10 18:08:36 +0500

    configures redux store, creates header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+import authService from "./appwrite/auth";
+import { Footer, Header } from "./components";
+import { login, logout } from "./redux/authSlice";
+
 function App() {
-  return (
-    <>
-      <div className="text-3xl text-white">Simple Blog App</div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login({ userData }));
+        else dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        TODO:{" "}
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
