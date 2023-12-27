e9a2840787a4d6bf197a4082cc7e84565348ef57
Imran Ahmed <iam.imran@yahoo.com>
2023-12-27 14:47:23 +0500

    moves LogoutButton component to its own directory, creates Login form component

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -10,7 +10,10 @@ function LogoutButton() {
     });
   };
   return (
-    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+    <button
+      onClick={logoutHandler}
+      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+    >
       Logout
     </button>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,10 @@ import Button from "./Button/Button";
 import Container from "./Container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-import LogoutButton from "./Header/LogoutButton";
 import Input from "./Input/Input";
+import Login from "./Login/Login";
 import Logo from "./Logo/Logo";
+import LogoutButton from "./LogoutButton/LogoutButton";
 import PostCard from "./PostCard/PostCard";
 import Select from "./Select/Select";
 
@@ -14,6 +15,7 @@ export {
   Footer,
   Header,
   Input,
+  Login,
   Logo,
   LogoutButton,
   PostCard,
