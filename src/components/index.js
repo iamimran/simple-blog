b9801690d6abf069427b4141e316f07586cd13a2
Imran Ahmed <iam.imran@yahoo.com>
2023-12-27 13:16:24 +0500

    adds PostCard and Select components

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,17 @@ import Header from "./Header/Header";
 import LogoutButton from "./Header/LogoutButton";
 import Input from "./Input/Input";
 import Logo from "./Logo/Logo";
+import PostCard from "./PostCard/PostCard";
+import Select from "./Select/Select";
 
-export { Button, Container, Footer, Header, Input, Logo, LogoutButton };
+export {
+  Button,
+  Container,
+  Footer,
+  Header,
+  Input,
+  Logo,
+  LogoutButton,
+  PostCard,
+  Select,
+};
